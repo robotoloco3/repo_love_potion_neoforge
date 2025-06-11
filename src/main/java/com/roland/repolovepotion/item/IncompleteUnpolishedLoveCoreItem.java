aeb35a9b633ebb36b69ddfbb476c23399b2f4cec
robotoloco3 <roland@shikoski.com>
2025-06-10 21:35:39 -0500

    mess up

--- src/main/java/com/roland/repolovepotion/item/IncompleteUnpolishedLoveCoreItem.java
+++ src/main/java/com/roland/repolovepotion/item/IncompleteUnpolishedLoveCoreItem.java
@@ -2,11 +2,38 @@
 package com.roland.repolovepotion.item;
 
 
+import com.simibubi.create.AllDataComponents;
 import com.simibubi.create.content.processing.sequenced.SequencedAssemblyItem;
-import net.minecraft.world.item.Rarity;
+import net.createmod.catnip.theme.Color;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.Objects;
 
 public class IncompleteUnpolishedLoveCoreItem extends SequencedAssemblyItem {
 	public IncompleteUnpolishedLoveCoreItem() {
-		super(new Properties().stacksTo(64).rarity(Rarity.UNCOMMON));
+		super(new Item.Properties().stacksTo(1));
 	}
+
+	public float getProgress(ItemStack stack) {
+		if (!stack.has(AllDataComponents.SEQUENCED_ASSEMBLY))
+			return 0;
+		return Objects.requireNonNull(stack.get(AllDataComponents.SEQUENCED_ASSEMBLY)).progress();
+	}
+
+	@Override
+	public boolean isBarVisible(ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	public int getBarWidth(ItemStack stack) {
+		return Math.round(getProgress(stack) * 13);
+	}
+
+	@Override
+	public int getBarColor(ItemStack stack) {
+		return Color.mixColors(0xFF_FFC074, 0xFF_46FFE0, getProgress(stack));
+	}
+
 }
