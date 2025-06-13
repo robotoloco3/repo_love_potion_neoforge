2c62f68f7337b7061c2498b9099b142eae53e2e9
robotoloco3 <roland@shikoski.com>
2025-06-12 20:57:56 -0500

    recipe

--- src/main/java/com/roland/repolovepotion/init/RepoLovePotionModItems.java
+++ src/main/java/com/roland/repolovepotion/init/RepoLovePotionModItems.java
@@ -2,6 +2,7 @@
 package com.roland.repolovepotion.init;
 
 import com.roland.repolovepotion.item.*;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.DeferredItem;
 
@@ -11,6 +12,7 @@ import com.roland.repolovepotion.RepoLovePotionMod;
 
 public class RepoLovePotionModItems {
 	public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(RepoLovePotionMod.MODID);
+
 	public static final DeferredItem<Item> LOVE_POTION = REGISTRY.register("love_potion", LovePotionItem::new);
 	public static final DeferredItem<Item> LOVE_CORE = REGISTRY.register("love_core", LoveCoreItem::new);
 	public static final DeferredItem<Item> HEART_JAR = REGISTRY.register("heart_jar", HeartJarItem::new);
@@ -19,5 +21,6 @@ public class RepoLovePotionModItems {
 	public static final DeferredItem<Item> I_LOVE_DISC = REGISTRY.register("i_love_disc", ILoveDiscItem::new);
 	public static final DeferredItem<Item> ARK_DISC = REGISTRY.register("ark_disc", ArkDiscItem::new);
 	public static final DeferredItem<Item> LOVE_DUST = REGISTRY.register("love_dust", LoveDustItem::new);
-	public static final DeferredItem<Item> INCOMPLETE_UNPOLISHED_LOVE_CORE = REGISTRY.register("incomplete_unpolished_love_core", IncompleteUnpolishedLoveCoreItem::new);
+	public static final DeferredItem<Item> INCOMPLETE_UNPOLISHED_LOVE_CORE = REGISTRY.register("incomplete_unpolished_love_core", () -> new IncompleteUnpolishedLoveCoreItem(new Item.Properties()));
+
 }
--- src/main/java/com/roland/repolovepotion/item/IncompleteUnpolishedLoveCoreItem.java
+++ src/main/java/com/roland/repolovepotion/item/IncompleteUnpolishedLoveCoreItem.java
@@ -1,39 +1,12 @@
 
 package com.roland.repolovepotion.item;
 
-
-import com.simibubi.create.AllDataComponents;
 import com.simibubi.create.content.processing.sequenced.SequencedAssemblyItem;
-import net.createmod.catnip.theme.Color;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-
-import java.util.Objects;
 
 public class IncompleteUnpolishedLoveCoreItem extends SequencedAssemblyItem {
-	public IncompleteUnpolishedLoveCoreItem() {
-		super(new Item.Properties().stacksTo(1));
-	}
+	public IncompleteUnpolishedLoveCoreItem(Properties properties) {
+		super(properties);
 
-	public float getProgress(ItemStack stack) {
-		if (!stack.has(AllDataComponents.SEQUENCED_ASSEMBLY))
-			return 0;
-		return Objects.requireNonNull(stack.get(AllDataComponents.SEQUENCED_ASSEMBLY)).progress();
 	}
-
-	@Override
-	public boolean isBarVisible(ItemStack stack) {
-		return true;
-	}
-
-	@Override
-	public int getBarWidth(ItemStack stack) {
-		return Math.round(getProgress(stack) * 13);
-	}
-
-	@Override
-	public int getBarColor(ItemStack stack) {
-		return Color.mixColors(0xFF_FFC074, 0xFF_46FFE0, getProgress(stack));
-	}
-
 }
