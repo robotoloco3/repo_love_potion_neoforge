cff66562932e01c8c914ff51055bd62f53afd112
robotoloco3 <roland@shikoski.com>
2025-06-18 17:03:49 -0500

    taxman

--- src/main/java/com/roland/repolovepotion/init/RepoLovePotionModTrades.java
+++ src/main/java/com/roland/repolovepotion/init/RepoLovePotionModTrades.java
@@ -21,8 +21,36 @@ public class RepoLovePotionModTrades {
 					new BasicItemListing(new ItemStack(Items.EMERALD, 16), new ItemStack(Items.BREAD, 32), new ItemStack(RepoLovePotionModItems.I_LOVE_DISC.get()), 10, 0, 0.05f));
 			event.getTrades().get(1).add(
 					new BasicItemListing(new ItemStack(Blocks.LANTERN, 2), new ItemStack(Items.EMERALD, 10), new ItemStack(Blocks.LANTERN), 10, 5, 0.2f));
-			event.getTrades().get(4).add(
+			event.getTrades().get(3).add(
 					new BasicItemListing(new ItemStack(RepoLovePotionModItems.I_LOVE_DISC.get()), new ItemStack(AllItems.STURDY_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.DEMONS_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.DIAMONDS_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.DOUBLE_LIFE_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.FLOWERS_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.GANGSTA_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.HYMN_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.JAGGER_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.LEVITATING_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.RULE_WORLD_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.SUNFLOWER_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.THUNDER_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.UPTOWN_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.VIVA_DISC.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(
+					new BasicItemListing(new ItemStack(RepoLovePotionModItems.VILLAGER_RAP_DISC.get()), new ItemStack(AllItems.GOLDEN_SHEET.get(), 10), new ItemStack(RepoLovePotionModItems.WORRIED_DISC.get()), 10, 5, 0.05f));
 		}
 	}
 }
