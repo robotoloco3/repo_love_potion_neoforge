f73da8f76fcd64adaf9d38a1b06be4c6b7e0d188
Lozi-Buster <Lazzylett@gmail>
2025-06-18 15:29:09 -0500

    add files

--- src/main/java/com/roland/repolovepotion/init/RepoLovePotionModItems.java
+++ src/main/java/com/roland/repolovepotion/init/RepoLovePotionModItems.java
@@ -21,5 +21,17 @@ public class RepoLovePotionModItems {
 	public static final DeferredItem<Item> ARK_DISC = REGISTRY.register("ark_disc", ArkDiscItem::new);
 	public static final DeferredItem<Item> LOVE_DUST = REGISTRY.register("love_dust", LoveDustItem::new);
 	public static final DeferredItem<Item> INCOMPLETE_UNPOLISHED_LOVE_CORE = REGISTRY.register("incomplete_unpolished_love_core", () -> new IncompleteUnpolishedLoveCoreItem(new Item.Properties()));
-
-}
+	public static final DeferredItem<Item> DEMONS_DISC = REGISTRY.register("demons_disc", DemonsDiscItem::new);
+	public static final DeferredItem<Item> DIAMONDS_DISC = REGISTRY.register("diamonds_disc", DiamondsDiscItem::new);
+	public static final DeferredItem<Item> DOUBLELIFE_DISC = REGISTRY.register("double_life_disc", DoubleLifeDiscItem::new);
+	public static final DeferredItem<Item> RULEWORLD_DISC = REGISTRY.register("rule_world_disc", RuleWorldDiscItem::new);
+	public static final DeferredItem<Item> FLOWERS_DISC = REGISTRY.register("flowers_disc", FlowersDiscItem::new);
+	public static final DeferredItem<Item> GANGSTA_DISC = REGISTRY.register("gangsta_disc", GangstaDiscItem::new);
+	public static final DeferredItem<Item> HYMN_DISC = REGISTRY.register("hymn_disc", HymnDiscItem::new);
+	public static final DeferredItem<Item> WORRIED_DISC = REGISTRY.register("worried_disc", WorriedDiscItem::new);
+	public static final DeferredItem<Item> LEVITATING_DISC = REGISTRY.register("levitating_disc", LevitatingDiscItem::new);
+	public static final DeferredItem<Item> JAGGER_DISC = REGISTRY.register("jagger_disc", JaggerDiscItem::new);
+	public static final DeferredItem<Item> SUNFLOWER_DISC = REGISTRY.register("sunflower_disc", SunflowerDiscItem::new);
+	public static final DeferredItem<Item> THUNDER_DISC = REGISTRY.register("thunder_disc", ThunderDiscItem::new);
+	public static final DeferredItem<Item> UPTOWN_DISC = REGISTRY.register("uptown_disc", UptownDiscItem::new);
+	public static final DeferredItem<Item> VIVA_DISC = REGISTRY.register("viva_disc", VivaDiscItem::new);
